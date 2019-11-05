2b7c4b1722d8ca09215855706e81b15cc3003879
Nirvach <Nirvach@github.com>
2019-11-05 22:03:50 +0300

    update

--- src/LetterGame.java
+++ src/LetterGame.java
@@ -9,13 +9,22 @@ public class LetterGame {
         char input;
 
         do {
-            System.out.println("Please type any char");
-            System.out.println("Correct letter - " + randomChar);
+          //System.out.println("Correct letter - " + randomChar);
             input = sc.next().charAt(0);
-            if ((int)input > 90) {
-                input = (char)(input - 32);
+            if (!Character.UnicodeBlock.CYRILLIC.equals(Character.UnicodeBlock.of(input))) {
+
+                if ((input - 32) < randomChar) {
+                    System.out.println("too low");
+                } else if ((input - 32) > randomChar) {
+                    System.out.println("too high");
+                }
+                if ((int) input > 90) {
+                    input = (char) (input - 32);
+                }
+            } else {
+                System.out.println("Using cyrillic!!!");
             }
-        } while   (input != randomChar );
+        } while (input != randomChar );
 
         System.out.println("Correct!");
     }
